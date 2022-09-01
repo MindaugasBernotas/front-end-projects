183ea1902d86534f0c7c8d4536052212ee8333fe
Mindaugas <dizainas@gmail.com>
2022-09-01 12:25:18 +0300

    bootstrap

---
+++ ruoniai/005/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import Select from "./Select";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm">One of three columns</div>
+            <div className="col-sm">One of three columns</div>
+            <div className="col-sm">One of three columns</div>
+          </div>
+        </div>
+
+        <h1>FORMS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
